06c9308170f8ef0b066c1f57443b3346801aa6fd
Dineshk-A <dineshgamer1019@gmail.com>
2025-01-11 14:54:57 +0530

    typo

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Hero } from './components/Hero';
 import { Skills } from './components/Skills';
 import { Projects } from './components/Projects';
 import { Contact } from './components/Contact';
-import { Experience } from "./Experience";
+import { Experience } from "./components/Experience";
 
 function App() {
   return (
